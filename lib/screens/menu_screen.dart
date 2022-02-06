bc4999389fac948e42bcef0a91fb577286ff63d3
Pradeep Shah <pradeep.shah@kcl.ac.uk>
2022-02-06 16:59:00 +0000

    Create menu screen and add tests

--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -19,40 +19,173 @@ class _MenuScreenState extends State<MenuScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Read That Sheet'),
-      ),
       body: SafeArea(
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              'HOME PAGE',
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.fromLTRB(5.0, 10.0, 5.0, 5.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.indigo.shade300,
+                ),
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        'Read\n That\n Sheet',
+                        style: TextStyle(
+                            fontSize: 100.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey.shade300,
+                            shadows: [
+                              Shadow(
+                                color: Colors.blueGrey.shade800,
+                                offset: const Offset(5.0, 5.0),
+                              ),
+                            ]),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            ElevatedButton(
-              key: const Key('LessonButton'),
-              onPressed: () {
-                Navigator.pushNamed(context, LessonScreen.id);
-              },
-              child: const Text('Lessons'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, PracticeScreen.id);
-              },
-              child: const Text('Practice'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, AchievementsScreen.id);
-              },
-              child: const Text('Achievements'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, SettingsScreen.id);
-              },
-              child: const Text('Settings'),
+            Expanded(
+              child: Column(
+                children: [
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, LessonScreen.id);
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.fromLTRB(5.0, 10.0, 5.0, 2.5),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: Colors.indigo.shade400,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                'Lessons',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 40.0,
+                                  color: Colors.orange.shade500,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, PracticeScreen.id);
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.fromLTRB(5.0, 2.5, 5.0, 2.5),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: Colors.indigo.shade400,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                'Practice',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 40.0,
+                                  color: Colors.orange.shade500,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, AchievementsScreen.id);
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.fromLTRB(5.0, 2.5, 5.0, 2.5),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: Colors.indigo.shade400,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  'Achievements',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 40.0,
+                                    color: Colors.orange.shade500,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Container(),
+                          flex: 4,
+                        ),
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.pushNamed(context, SettingsScreen.id);
+                            },
+                            child: Container(
+                              margin:
+                                  const EdgeInsets.fromLTRB(0.0, 2.5, 5.0, 5.0),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10.0),
+                                color: Colors.indigo.shade400,
+                              ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    Icons.settings,
+                                    size: 75.0,
+                                    color: Colors.orange.shade300,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          flex: 2,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ],
         ),
