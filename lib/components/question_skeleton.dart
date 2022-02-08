c60b83226e24d22564aac34494a6a25e73f11df2
Nannaphat Keamanuchet <nannaphat.keamanuchet@kcl.ac.uk>
2022-02-08 21:32:00 +0000

    Add final mainColor and refactor code
    Delete unnecessary import and add constructor

--- lib/components/question_skeleton.dart
+++ lib/components/question_skeleton.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 //fix later
 //watch a video on how to make an app have many pages
@@ -7,11 +6,15 @@ import 'package:flutter/services.dart';
 // be careful about const
 class QuestionSkeleton extends StatefulWidget {
   static String id = 'question_skeleton';
+
+  const QuestionSkeleton({Key? key}) : super(key: key);
+
   @override
   _QuestionSkeletonState createState() => _QuestionSkeletonState();
 }
 
 class _QuestionSkeletonState extends State<QuestionSkeleton> {
+  //Not sure if I should keep it
   @override
   void initState() {
     super.initState();
@@ -22,19 +25,17 @@ class _QuestionSkeletonState extends State<QuestionSkeleton> {
     super.dispose();
   }
 
-//I want to format these buttons even further
+  final Color mainColor = Colors.orange;
+
   Expanded buildChoiceButton(String choice, MaterialColor color) {
     return Expanded(
-      flex: 1, //container
-      //wrap in gesture detector
-      //or wrap textbutton in container and add margin
+      flex: 1,
       child: Container(
         margin: const EdgeInsets.all(15.0),
         child: TextButton(
           onPressed: () {},
           style: TextButton.styleFrom(
-            backgroundColor: Colors.orange,
-            maximumSize: const Size(70, 70),
+            backgroundColor: mainColor,
           ),
           child: Text(choice),
         ),
@@ -54,32 +55,34 @@ class _QuestionSkeletonState extends State<QuestionSkeleton> {
           padding: const EdgeInsets.all(8.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            //crossAxisAlignment: CrossAxisAlignment.stretch,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               //question number
               const Expanded(
                 child: Text('Question 1/1'),
               ),
+
+              //picture and question text
               Row(
-                //picture and question text
                 children: const <Widget>[
                   Expanded(
-                      child: Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Text('Picture'),
-                  )),
+                    child: Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Text('Picture'),
+                    ),
+                  ),
                   //Add text size
                   Expanded(
-                      child: Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Text(
-                        'Question 1: What is the letter that is used to represent "Do"?'),
-                  )),
+                    child: Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Text(
+                          'Question 1: What is the letter that is used to represent "Do"?'),
+                    ),
+                  ),
                 ],
               ),
 
-              //choices
-
+              //choices buttons
               Expanded(
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
