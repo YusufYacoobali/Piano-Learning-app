05ad152686c4d37c49a1acdc94215798570b7e7f
Pradeep Shah <pradeep.shah@kcl.ac.uk>
2022-02-13 15:25:35 +0000

    Create white keys on keyboard

--- lib/screens/keyboard_screen.dart
+++ lib/screens/keyboard_screen.dart
@@ -11,19 +11,61 @@ class _KeyboardScreenState extends State<KeyboardScreen> {
     super.dispose();
   }
 
+  Widget getWhiteKey(String buttonText) {
+    return Expanded(
+      child: ElevatedButton(
+        child: Column(
+          children: [
+            const Spacer(),
+            Text(
+              buttonText,
+              style: const TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+              ),
+              textAlign: TextAlign.right,
+            ),
+          ],
+        ),
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          primary: Colors.white,
+          minimumSize: const Size(double.infinity, double.infinity),
+          padding: const EdgeInsets.all(20.0),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> getWhiteKeys() {
+    List<Widget> whiteKeys = [];
+    List<String> notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+    for (int i = 0; i < notes.length; ++i) {
+      whiteKeys.add(getWhiteKey(notes[i]));
+    }
+    return whiteKeys;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Column(
           children: [
-            const Text(
-              'KEYBOARD PAGE',
+            Expanded(
+              flex: 5,
+              // TODO: Add music sheet to play on keyboard
+              child: Container(),
             ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('EMPTY'),
+            Expanded(
+              flex: 3,
+              child: Container(
+                color: Colors.black,
+                child: Row(
+                  children: getWhiteKeys(),
+                ),
+              ),
             ),
           ],
         ),
@@ -40,3 +82,17 @@ class KeyboardScreen extends StatefulWidget {
   @override
   _KeyboardScreenState createState() => _KeyboardScreenState();
 }
+
+// Old template code
+// Row(
+// mainAxisAlignment: MainAxisAlignment.center,
+// children: [
+// const Text(
+// 'KEYBOARD PAGE',
+// ),
+// ElevatedButton(
+// onPressed: () {},
+// child: const Text('EMPTY'),
+// ),
+// ],
+// )
