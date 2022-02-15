d671c5a2f15705a5a99d7b09e80c19770bced9b1
yusuf.yacoobali <yyacoobali@gmail.com>
2022-02-15 10:16:46 +0000

    delete dead code

--- lib/components/achievement_components/achievements_completed.dart
+++ lib/components/achievement_components/achievements_completed.dart
@@ -10,29 +10,12 @@ class AchievementsCompleted extends StatelessWidget {
   const AchievementsCompleted({Key? key, required this.cards})
       : super(key: key);
 
-  // static const List<List> inProgressText = [
-  //   ['Open the app once', 1, 1],
-  //   ['Go to achievements screen 5 times', 5, 5],
-  //   ['realllllllyyy lllooooooooooonggg     achievement dweidjeudh32udh', 1, 1],
-  //   ['Complete 5 quizzes', 5, 5],
-  //   ['blah blaj', 5, 5],
-  //   ['blahhhh', 5, 5],
-  // ];
-
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: Row(
-        children: [
-          // for (var item in inProgressText)
-          //   AchievementCard(
-          //     text: item[0],
-          //     complete: item[1],
-          //     target: item[2],
-          //   )
-          for (AchievementCard card in cards) card
-        ],
+        children: [for (AchievementCard card in cards) card],
       ),
     );
   }
