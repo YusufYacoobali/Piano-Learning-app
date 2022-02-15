aa0cf01a25c095b24177823eb73411562c3d25db
“Yingtong <yingtong.hu@kcl.ac.uk>
2022-02-15 10:20:04 +0000

    Initialise Question and create list of question objects.

--- lib/question.dart
+++ lib/question.dart
@@ -1,1 +1,12 @@
+class Question {
+  final String image;
+  final String question;
+  final String correctAnswer;
+//options;
 
+  Question({
+    required this.image,
+    required this.question,
+    required this.correctAnswer,
+  });
+}
