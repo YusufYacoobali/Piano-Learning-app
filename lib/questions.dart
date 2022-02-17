ebcba779d32b1227a4101cdb247aeb0c2df8534f
“Yingtong <yingtong.hu@kcl.ac.uk>
2022-02-17 23:43:40 +0000

    Add new method for asset image path

--- lib/questions.dart
+++ lib/questions.dart
@@ -6,134 +6,140 @@ class Questions {
 
   final List<Question> _questions = [
     Question(
-      image: 'Bs_A',
+      image: 'Bs_A.jpeg',
       question: 'What note is this?',
       correctAnswer: 'A',
     ),
     Question(
-      image: 'Bs_B',
+      image: 'Bs_B.jpeg',
       question: 'What note is this?',
       correctAnswer: 'B',
     ),
     Question(
-      image: 'Bs_C',
+      image: 'Bs_C.jpeg',
       question: 'What note is this?',
       correctAnswer: 'C',
     ),
-    Question(
-      image: 'Bs_D',
+    /* Question(
+      image: 'Bs_D.jpeg',
       question: 'What note is this?',
       correctAnswer: 'D',
     ),
     Question(
-      image: 'Bs_E',
+      image: 'Bs_E.jpeg',
       question: 'What note is this?',
       correctAnswer: 'E',
     ),
     Question(
-      image: 'Bs_F',
+      image: 'Bs_F.jpeg',
       question: 'What note is this?',
       correctAnswer: 'F',
     ),
     Question(
-      image: 'Bs_G',
+      image: 'Bs_G.jpeg',
       question: 'What note is this?',
       correctAnswer: 'G',
     ),
     Question(
-      image: 'Bs_LowA',
+      image: 'Bs_LowA.jpeg',
       question: 'What note is this?',
       correctAnswer: 'A',
     ),
     Question(
-      image: 'Bs_LowB',
+      image: 'Bs_LowB.jpeg',
       question: 'What note is this?',
       correctAnswer: 'B',
     ),
     Question(
-      image: 'Bs_LowF',
+      image: 'Bs_LowF.jpeg',
       question: 'What note is this?',
       correctAnswer: 'F',
     ),
     Question(
-      image: 'Bs_LowG',
+      image: 'Bs_LowG.jpeg',
       question: 'What note is this?',
       correctAnswer: 'G',
     ),
     Question(
-      image: 'Bs_MidC',
+      image: 'Bs_MidC.jpeg',
       question: 'What note is this?',
       correctAnswer: 'C',
     ),
     Question(
-      image: 'Tr_A',
+      image: 'Tr_A.jpeg',
       question: 'What note is this?',
       correctAnswer: 'A',
     ),
     Question(
-      image: 'Tr_B',
+      image: 'Tr_B.jpeg',
       question: 'What note is this?',
       correctAnswer: 'B',
     ),
     Question(
-      image: 'Tr_C',
+      image: 'Tr_C.jpeg',
       question: 'What note is this?',
       correctAnswer: 'C',
     ),
     Question(
-      image: 'Tr_D',
+      image: 'Tr_D.jpeg',
       question: 'What note is this?',
       correctAnswer: 'D',
     ),
     Question(
-      image: 'Tr_E',
+      image: 'Tr_E.jpeg',
       question: 'What note is this?',
       correctAnswer: 'E',
     ),
     Question(
-      image: 'Tr_F',
+      image: 'Tr_F.jpeg',
       question: 'What note is this?',
       correctAnswer: 'F',
     ),
     Question(
-      image: 'Tr_G',
+      image: 'Tr_G.jpeg',
       question: 'What note is this?',
       correctAnswer: 'G',
     ),
     Question(
-      image: 'Tr_HighA',
+      image: 'Tr_HighA.jpeg',
       question: 'What note is this?',
       correctAnswer: 'A',
     ),
     Question(
-      image: 'Tr_HighD',
+      image: 'Tr_HighD.jpeg',
       question: 'What note is this?',
       correctAnswer: 'D',
     ),
     Question(
-      image: 'Tr_HighE',
+      image: 'Tr_HighE.jpeg',
       question: 'What note is this?',
       correctAnswer: 'E',
     ),
     Question(
-      image: 'Tr_HighF',
+      image: 'Tr_HighF.jpeg',
       question: 'What note is this?',
       correctAnswer: 'F',
     ),
     Question(
-      image: 'Tr_HighG',
+      image: 'Tr_HighG.jpeg',
       question: 'What note is this?',
       correctAnswer: 'G',
     ),
     Question(
-      image: 'Tr_MidC',
+      image: 'Tr_MidC.jpeg',
       question: 'What note is this?',
-      correctAnswer: 'C',
-    ),
+      correctAnswer: 'C', 
+    ),*/
   ];
 
   String getImageName() {
     return _questions[_questionNum].image;
+  }
+
+  String getImagePath() {
+    String path = 'assets/NoteImages/';
+    String fullImagePath = path + getImageName();
+    return fullImagePath;
   }
 
   String getQuestionsText() {
@@ -148,5 +154,13 @@ class Questions {
     if (_questionNum < _questions.length - 1) {
       _questionNum++;
     }
+  }
+
+  int getCurrentQuestionNum() {
+    return _questionNum;
+  }
+
+  int getTotalQuestionLength() {
+    return _questions.length;
   }
 }
