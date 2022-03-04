17fa224d04f18fd72cfc27ee52f4878722b0b235
Patel, Kyal (k20008595) <kyal.patel@kcl.ac.uk>
2022-03-04 09:49:12 +0000

    Add template files for managing tracks
    - Create file to store a list of all tracks
    - Add a template file for each track
    - Create file for track one (without note timing values)
    - Remove unused imports from play_along_screen

--- lib/screens/play_along_screen.dart
+++ lib/screens/play_along_screen.dart
@@ -1,23 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:sight_reading_app/constants.dart';
-import 'package:sight_reading_app/screens/results_screen.dart';
-import '../components/question_skeleton.dart';
-import 'package:sight_reading_app/question_brain.dart';
 
 /// The screen that runs the "play along" practice mode with a given track.
 ///
 /// The track is selected by the user, then passed in to this screen.
 class _PlayAlongScreenState extends State<PlayAlongScreen> {
-  /// The template used to show questions on the screen
-  late QuestionBrain questionBrain;
-  /// The main widget of the screen
-  late Widget screenWidget;
 
   @override
   void initState() {
     super.initState();
-    //questionBrain = QuestionBrain(questionList: lessonOneQuestions);
-    //setScreenWidget();
   }
 
   @override
