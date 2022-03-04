82582e74596ec39c9b6ca8f7b22df9447c27e0f2
Patel, Kyal (k20008595) <kyal.patel@kcl.ac.uk>
2022-03-04 13:23:09 +0000

    Add track list file
    - Also added files for other tracks

--- lib/play_along_tracks/tracks_list.dart
+++ lib/play_along_tracks/tracks_list.dart
@@ -1,5 +1,5 @@
 import 'package:sight_reading_app/play_along_tracks/track_template.dart';
-import 'package:sight_reading_app/play_along_tracks/track_one_happy_birthday.dart';
+import 'package:sight_reading_app/play_along_tracks/track_one.dart';
 
 ///A list of every track available to play along to.
 final List<Track> tracks = [
