5ab74a22c338cd62e3c65f2b5ad5495b8f060efc
Aaron Gomes <aaron.gomes@kcl.ac.uk>
2022-03-05 20:07:47 +0000

    Move keyboard into a component

--- lib/screens/keyboard_screen.dart
+++ lib/screens/keyboard_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 import '../constants.dart';
+import 'package:sight_reading_app/components/keyboard.dart';
 
 class _KeyboardScreenState extends State<KeyboardScreen> {
   final player = AudioCache();
@@ -16,165 +17,22 @@ class _KeyboardScreenState extends State<KeyboardScreen> {
   void dispose() {
     super.dispose();
   }
-
-  // Returns the text displayed on the white keys
-  Widget getWhiteKeyChild(String buttonText) {
-    return FittedBox(
-      fit: BoxFit.fitWidth,
-      child: Text(
-        buttonText,
-        style: whiteKeyTextStyle,
-        textAlign: TextAlign.right,
-      ),
-    );
-  }
-
-  // Returns the text displayed on the black keys
-  Widget getBlackKeyChild(String buttonText) {
-    return FittedBox(
-      fit: BoxFit.fitWidth,
-      child: Text(
-        buttonText,
-        style: blackKeyTextStyle,
-        textAlign: TextAlign.right,
-      ),
-    );
-  }
-
-  // Returns the buttons for the white keys
-  Widget getWhiteKey(String buttonText) {
-    return Expanded(
-      child: ElevatedButton(
-        child: Column(
-          children: [
-            // Ensures the name of the key is at the bottom
-            const Spacer(),
-            getWhiteKeyChild(buttonText),
-          ],
-        ),
-        onPressed: () {
-          playSound(buttonText.toLowerCase());
-        },
-        style: whiteKeyButtonStyle,
-      ),
-    );
-  }
-
-  // Returns the buttons for the black keys
-  Widget getBlackKey(String buttonText) {
-    return ElevatedButton(
-      child: Column(
-        children: [
-          // Ensures the name of the key is at the bottom
-          const Spacer(),
-          getBlackKeyChild(buttonText),
-        ],
-      ),
-      onPressed: () {
-        playSound(buttonText.toLowerCase());
-      },
-      style: blackKeyButtonStyle,
-    );
-  }
-
-  // Returns the list of white keys
-  List<Widget> getWhiteKeys() {
-    List<Widget> whiteKeys = [];
-    List<String> notes = whiteKeyNames;
-    for (int i = 0; i < notes.length; ++i) {
-      whiteKeys.add(getWhiteKey(notes[i]));
-    }
-    return whiteKeys;
-  }
-
-  // Returns an empty expanded used to create a gap between the black keys
-  Widget getBlackKeySpace(int flex) {
-    return Expanded(
-      flex: flex,
-      child: Container(),
-    );
-  }
-
-  // Returns the list of black keys with the gaps in between them
-  List<Widget> getBlackKeys() {
-    List<String> notes = blackKeyNames;
-    List<Widget> blackKeys = [
-      getBlackKeySpace(4),
-      // C# or Db key
-      Expanded(
-        flex: 3,
-        child: getBlackKey(notes[0]),
-      ),
-      getBlackKeySpace(3),
-      // D# or Eb key
-      Expanded(
-        flex: 3,
-        child: getBlackKey(notes[1]),
-      ),
-      getBlackKeySpace(8),
-      // F# or Gb key
-      Expanded(
-        flex: 3,
-        child: getBlackKey(notes[2]),
-      ),
-      getBlackKeySpace(3),
-      // G# or Ab key
-      Expanded(
-        flex: 3,
-        child: getBlackKey(notes[3]),
-      ),
-      getBlackKeySpace(3),
-      // A# or Bb key
-      Expanded(
-        flex: 3,
-        child: getBlackKey(notes[4]),
-      ),
-      getBlackKeySpace(4),
-    ];
-    return blackKeys;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(
-          children: [
-            Expanded(
-              flex: 5,
-              // TODO: Add music sheet to play on keyboard
-              child: Container(),
-            ),
-            Expanded(
-              flex: 3,
-              child: Container(
-                color: Colors.black,
-                // The black keys are stacked on top of the white keys
-                child: Stack(
-                  children: [
-                    Row(
-                      children: getWhiteKeys(),
-                    ),
-                    Column(
-                      children: [
-                        Expanded(
-                          flex: 6,
-                          child: Row(
-                            children: getBlackKeys(),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 5,
-                          child: Container(),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
+        children: [
+        Expanded(
+        flex: 5,
+          child: Container()
         ),
+        Expanded(
+        flex: 3,
+        child: Keyboard().build(context),
+      ),
+        ],
+      ),
       ),
     );
   }
--- lib/screens/keyboard_sheet_screen.dart
+++ lib/screens/keyboard_sheet_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
+import '../components/keyboard.dart';
 import '../components/sheet_music_components/music_sheet.dart';
 import '../components/sheet_music_components/note.dart';
 import '../components/sheet_music_components/progress_timer.dart';
@@ -32,123 +33,6 @@ class KeyboardSheetScreenState extends State<KeyboardSheetScreen> {
   void dispose() {
     super.dispose();
     _timer.stop();
-  }
-
-  // Returns the text displayed on the white keys
-  Widget getWhiteKeyChild(String buttonText) {
-    return FittedBox(
-      fit: BoxFit.fitWidth,
-      child: Text(
-        buttonText,
-        style: whiteKeyTextStyle,
-        textAlign: TextAlign.right,
-      ),
-    );
-  }
-
-  // Returns the text displayed on the black keys
-  Widget getBlackKeyChild(String buttonText) {
-    return FittedBox(
-      fit: BoxFit.fitWidth,
-      child: Text(
-        buttonText,
-        style: blackKeyTextStyle,
-        textAlign: TextAlign.right,
-      ),
-    );
-  }
-
-  // Returns the buttons for the white keys
-  Widget getWhiteKey(String buttonText) {
-    return Expanded(
-      child: ElevatedButton(
-        child: Column(
-          children: [
-            // Ensures the name of the key is at the bottom
-            const Spacer(),
-            getWhiteKeyChild(buttonText),
-          ],
-        ),
-        onPressed: () {
-          playSound(buttonText.toLowerCase());
-        },
-        style: whiteKeyButtonStyle,
-      ),
-    );
-  }
-
-  // Returns the buttons for the black keys
-  Widget getBlackKey(String buttonText) {
-    return ElevatedButton(
-      child: Column(
-        children: [
-          // Ensures the name of the key is at the bottom
-          const Spacer(),
-          getBlackKeyChild(buttonText),
-        ],
-      ),
-      onPressed: () {
-        playSound(buttonText.toLowerCase());
-      },
-      style: blackKeyButtonStyle,
-    );
-  }
-
-  // Returns the list of white keys
-  List<Widget> getWhiteKeys() {
-    List<Widget> whiteKeys = [];
-    List<String> notes = whiteKeyNames;
-    for (int i = 0; i < notes.length; ++i) {
-      whiteKeys.add(getWhiteKey(notes[i]));
-    }
-    return whiteKeys;
-  }
-
-  // Returns an empty expanded used to create a gap between the black keys
-  Widget getBlackKeySpace(int flex) {
-    return Expanded(
-      flex: flex,
-      child: Container(),
-    );
-  }
-
-  // Returns the list of black keys with the gaps in between them
-  List<Widget> getBlackKeys() {
-    List<String> notes = blackKeyNames;
-    List<Widget> blackKeys = [
-      getBlackKeySpace(4),
-      // C# or Db key
-      Expanded(
-        flex: 3,
-        child: getBlackKey(notes[0]),
-      ),
-      getBlackKeySpace(3),
-      // D# or Eb key
-      Expanded(
-        flex: 3,
-        child: getBlackKey(notes[1]),
-      ),
-      getBlackKeySpace(8),
-      // F# or Gb key
-      Expanded(
-        flex: 3,
-        child: getBlackKey(notes[2]),
-      ),
-      getBlackKeySpace(3),
-      // G# or Ab key
-      Expanded(
-        flex: 3,
-        child: getBlackKey(notes[3]),
-      ),
-      getBlackKeySpace(3),
-      // A# or Bb key
-      Expanded(
-        flex: 3,
-        child: getBlackKey(notes[4]),
-      ),
-      getBlackKeySpace(4),
-    ];
-    return blackKeys;
   }
 
   @override
@@ -193,47 +77,23 @@ class KeyboardSheetScreenState extends State<KeyboardSheetScreen> {
       ),
       body: SafeArea(
         child: Column(
-          children: [
-            Expanded(
-              flex: 5,
-              child: Scaffold(
-                body: CustomPaint(
-                  painter: _sheet,
-                  child: Container(),
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 3,
-              child: Container(
-                color: Colors.black,
-                // The black keys are stacked on top of the white keys
-                child: Stack(
-                  children: [
-                    Row(
-                      children: getWhiteKeys(),
-                    ),
-                    Column(
-                      children: [
-                        Expanded(
-                          flex: 6,
-                          child: Row(
-                            children: getBlackKeys(),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 5,
-                          child: Container(),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
+        children: [
+        Expanded(
+        flex: 5,
+        child: Scaffold(
+          body: CustomPaint(
+            painter: _sheet,
+            child: Container(),
+          ),
         ),
       ),
+      Expanded(
+        flex: 3,
+        child: Keyboard().build(context),
+      ),
+        ],
+    ),
+    ),
     );
   }
 }
