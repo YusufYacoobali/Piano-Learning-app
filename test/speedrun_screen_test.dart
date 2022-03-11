d6ac246e611c18d32515615db47778c2d028bd5b
yusuf.yacoobali <yyacoobali@gmail.com>
2022-03-11 09:39:56 +0000

    add more tests to speedrun

--- test/speedrun_screen_test.dart
+++ test/speedrun_screen_test.dart
@@ -1,24 +1,64 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:math';
+import 'package:fake_async/fake_async.dart';
+
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sight_reading_app/main.dart';
+import 'package:sight_reading_app/screens/results_screen.dart';
+import 'package:sight_reading_app/screens/speedrun_screen.dart';
 
 void main() {
-  _goToSpeedrunScreen(WidgetTester tester) async {
-    await tester.pumpWidget(const SightReadingApp());
-    await tester.tap(find.text('Practice'));
-    await tester.pumpAndSettle();
-    await tester.tap(find.text('Speedrun'));
-    await tester.pumpAndSettle();
-    await tester.tap(find.text('10 seconds'));
-    await tester.pumpAndSettle();
-  }
-
-  testWidgets('Check that the correct page is shown',
-      (WidgetTester tester) async {
-    await _goToSpeedrunScreen(tester);
-    await tester.pumpAndSettle();
-    //expect(find.text('D'), findsOneWidget);
+  testWidgets('Check that the timer is displayed', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: SpeedrunScreen(timerDuration: 10),
+      ),
+    );
     expect(find.byType(CircularCountDownTimer), findsOneWidget);
     expect(find.text('D'), findsOneWidget);
   });
+
+  testWidgets('Check that the options are displayed',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: SpeedrunScreen(timerDuration: 10),
+      ),
+    );
+    expect(find.text('A'), findsOneWidget);
+    expect(find.text('B'), findsOneWidget);
+    expect(find.text('C'), findsOneWidget);
+    expect(find.text('D'), findsOneWidget);
+    expect(find.text('E'), findsOneWidget);
+    expect(find.text('F'), findsOneWidget);
+    expect(find.text('G'), findsOneWidget);
+  });
+
+//  TODO: unsure of how to make it wait for 10 seconds so timer finishes
+  // testWidgets('Check that the results screen appears when timer is finished',
+  //     (WidgetTester tester) async {
+  //   await tester.pumpWidget(
+  //     const MaterialApp(
+  //       home: SpeedrunScreen(timerDuration: 10),
+  //     ),
+  //   );
+  //   //expect(find.byType(CircularCountDownTimer), findsOneWidget);
+  //   //await Future.delayed(const Duration(seconds: 12), () {});
+  //   //sleep(const Duration(seconds: 12));
+  //   // fakeAsync((async) {
+  //   //   // All asynchronous features that rely on timing are automatically
+  //   //   // controlled by [fakeAsync].
+  //   //   expect(new Completer().future.timeout(new Duration(seconds: 12)),
+  //   //       throwsA(isInstanceOf<TimeoutException>()));
+
+  //   //   // This will cause the timeout above to fire immediately, without waiting
+  //   //   // 5 seconds of real time.
+  //   //   async.elapse(new Duration(seconds: 12));
+  //   // });
+
+  //   expect(find.text('Exit'), findsOneWidget);
+  // });
 }
