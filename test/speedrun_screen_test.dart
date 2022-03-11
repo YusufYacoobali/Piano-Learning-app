103110194d265d5ae23c4485cd89d271a0529912
yusuf.yacoobali <yyacoobali@gmail.com>
2022-03-11 09:40:33 +0000

    delete unused imports

--- test/speedrun_screen_test.dart
+++ test/speedrun_screen_test.dart
@@ -1,13 +1,6 @@
-import 'dart:async';
-import 'dart:io';
-import 'dart:math';
-import 'package:fake_async/fake_async.dart';
-
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:sight_reading_app/main.dart';
-import 'package:sight_reading_app/screens/results_screen.dart';
 import 'package:sight_reading_app/screens/speedrun_screen.dart';
 
 void main() {
