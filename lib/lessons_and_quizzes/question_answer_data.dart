191a1ac101cc4fd65378ef65ebfac4d82c2bbaaf
Pradeep Shah <pradeep.shah@kcl.ac.uk>
2022-03-12 15:49:23 +0000

    Create QuestionAnswerData
    - Add getPracticeQuestions method to QuestionFinder
    - Create non-functional QuestionAnswerData class

---
+++ lib/lessons_and_quizzes/question_answer_data.dart
@@ -0,0 +1,5 @@
+class QuestionAnswerData {
+  static List<int> getPracticeQuestionIDs(int numOfQuestionIDs) {
+    return [1, 2, 3];
+  }
+}
--- lib/lessons_and_quizzes/question_finder.dart
+++ lib/lessons_and_quizzes/question_finder.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'package:sight_reading_app/lessons_and_quizzes/question_answer_data.dart';
 import 'package:sight_reading_app/questions.dart';
 import '../question.dart';
 
@@ -22,5 +23,22 @@ class QuestionFinder {
       lessonQuestions.add(questionsToPickFrom.removeAt(randomIndex));
     }
     return lessonQuestions;
+  }
+
+  List<Question> getPracticeQuestions(int numOfQuestions) {
+    // TODO: Make more efficient
+    List<Question> practiceQuestions = [];
+    List<int> questionIDs =
+        QuestionAnswerData.getPracticeQuestionIDs(numOfQuestions);
+
+    for (int id in questionIDs) {
+      for (Question question in questions) {
+        if (question.questionID == id) {
+          practiceQuestions.add(question);
+        }
+      }
+    }
+
+    return practiceQuestions;
   }
 }
