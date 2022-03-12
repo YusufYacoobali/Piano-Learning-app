0a4ba8bd22d405a98fb12c220e70051df82eef0f
Pradeep Shah <pradeep.shah@kcl.ac.uk>
2022-03-12 16:04:51 +0000

    Add questionAnswered method

--- lib/lessons_and_quizzes/question_answer_data.dart
+++ lib/lessons_and_quizzes/question_answer_data.dart
@@ -1,5 +1,25 @@
 class QuestionAnswerData {
+  // Need to load map from storage when app open
+  // Need to write map to storage when app close
+  static Map<int, int> _questionStatistics = {};
+
+  // TODO: Implement method
   static List<int> getPracticeQuestionIDs(int numOfQuestionIDs) {
+    // Go through map values in ascending order
+    // Add each corresponding ID to the return list
     return [1, 2, 3];
+  }
+
+  static void questionAnswered(int questionID, bool isCorrect) {
+    int? currentStatistic = _questionStatistics[questionID];
+    if (currentStatistic != null) {
+      if (isCorrect) {
+        _questionStatistics[questionID] = ++currentStatistic;
+      } else {
+        _questionStatistics[questionID] = --currentStatistic;
+      }
+    } else {
+      print('Invalid id');
+    }
   }
 }
