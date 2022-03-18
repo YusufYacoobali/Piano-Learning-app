51baef657a730212f7866142a42c2641d3cb2588
“Yingtong <yingtong.hu@kcl.ac.uk>
2022-03-18 10:33:01 +0000

    Created new files for adding note name and image name for future image assets.

---
+++ lib/components/helper_note_info_list.dart
@@ -0,0 +1,26 @@
+import '../components/helper_note_info.dart';
+
+class HelperNoteInfoList {
+  List<HelperNoteInfo> helperBassNoteImageNameList = [
+    HelperNoteInfo(noteImageName: 'Bs_A.jpeg', noteName: 'Bass A'),
+    HelperNoteInfo(noteImageName: 'Bs_B.jpeg', noteName: 'Bass B'),
+    HelperNoteInfo(noteImageName: 'Bs_C.jpeg', noteName: 'Bass C'),
+    HelperNoteInfo(noteImageName: 'Bs_D.jpeg', noteName: 'Bass D'),
+    HelperNoteInfo(noteImageName: 'Bs_E.jpeg', noteName: 'Bass E'),
+    HelperNoteInfo(noteImageName: 'Bs_F.jpeg', noteName: 'Bass F'),
+    HelperNoteInfo(noteImageName: 'Bs_G.jpeg', noteName: 'Bass G'),
+  ];
+
+  List<HelperNoteInfo> helperClefNoteImageNameList = [
+    HelperNoteInfo(noteImageName: 'Tr_A.jpeg', noteName: 'Clef A'),
+    HelperNoteInfo(noteImageName: 'Tr_B.jpeg', noteName: 'Clef B'),
+    HelperNoteInfo(noteImageName: 'Tr_C.jpeg', noteName: 'Clef C'),
+    HelperNoteInfo(noteImageName: 'Tr_D.jpeg', noteName: 'Clef D'),
+    HelperNoteInfo(noteImageName: 'Tr_E.jpeg', noteName: 'Clef E'),
+    HelperNoteInfo(noteImageName: 'Tr_F.jpeg', noteName: 'Clef F'),
+    HelperNoteInfo(noteImageName: 'Tr_G.jpeg', noteName: 'Clef G'),
+  ];
+
+  //maybe the soundtrack note list function as well
+
+}
