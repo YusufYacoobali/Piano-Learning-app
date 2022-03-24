bb58a5c36d778596eb07c2b4679b911482f49886
janiesrosario <janies.rosario@kcl.ac.uk>
2022-03-24 09:51:50 +0000

    fix initial page for keyboard

--- lib/components/page_keyboard.dart
+++ lib/components/page_keyboard.dart
@@ -16,7 +16,7 @@ class _PageKeyboardState extends State<PageKeyboard> {
 
   ///manages swipe detection
   final controller = PageController(
-    initialPage: 2,
+    initialPage: 1,
   );
 
   @override
