5deaa970bdad2106550cf88d717a1936b24d2dc0
“Yingtong <yingtong.hu@kcl.ac.uk>
2022-03-24 12:01:12 +0000

    Update the note sound file for helper

--- lib/components/helper/helper_bass_note_list.dart
+++ lib/components/helper/helper_bass_note_list.dart
@@ -8,44 +8,30 @@ final HelperList bassNoteImageNameList = HelperList(
     HelperNoteInfo(
         noteImageName: 'assets/note_images/Bs_A.jpeg',
         noteName: 'Bass A',
-        noteSound: 'note_a.wav'),
+        noteSound: 'note_low_a.wav'),
     HelperNoteInfo(
         noteImageName: 'assets/note_images/Bs_B.jpeg',
         noteName: 'Bass B',
-        noteSound: 'note_b.wav'),
+        noteSound: 'note_low_b.wav'),
     HelperNoteInfo(
         noteImageName: 'assets/note_images/Bs_C.jpeg',
         noteName: 'Bass C',
-        noteSound: 'note_c.wav'),
+        noteSound: 'note_low_c.wav'),
     HelperNoteInfo(
         noteImageName: 'assets/note_images/Bs_D.jpeg',
         noteName: 'Bass D',
-        noteSound: 'note_d.wav'),
+        noteSound: 'note_low_d.wav'),
     HelperNoteInfo(
         noteImageName: 'assets/note_images/Bs_E.jpeg',
         noteName: 'Bass E',
-        noteSound: 'note_e.wav'),
+        noteSound: 'note_low_e.wav'),
     HelperNoteInfo(
         noteImageName: 'assets/note_images/Bs_F.jpeg',
         noteName: 'Bass F',
-        noteSound: 'note_f.wav'),
+        noteSound: 'note_low_f.wav'),
     HelperNoteInfo(
         noteImageName: 'assets/note_images/Bs_G.jpeg',
         noteName: 'Bass G',
-        noteSound: 'note_g.wav'),
+        noteSound: 'note_low_g.wav'),
   ],
 );
-
-/*
-  static String getImagePath(index) {
-    return bassNoteImageNameList[index].noteImageName;
-  }
-
-  static String getImageName(index) {
-    return bassNoteImageNameList[index].noteName;
-  }
-
-  static String getNoteSound(index) {
-    return bassNoteImageNameList[index].noteSound;
-  }
-  */
--- lib/components/helper/helper_clef_note_list.dart
+++ lib/components/helper/helper_clef_note_list.dart
@@ -8,30 +8,30 @@ final HelperList clefNoteImageNameList = HelperList(
     HelperNoteInfo(
         noteImageName: 'assets/note_images/Tr_A.jpeg',
         noteName: 'Clef A',
-        noteSound: 'note_a.wav'),
+        noteSound: 'note_middle_a.wav'),
     HelperNoteInfo(
         noteImageName: 'assets/note_images/Tr_B.jpeg',
         noteName: 'Clef B',
-        noteSound: 'note_b.wav'),
+        noteSound: 'note_middle_b.wav'),
     HelperNoteInfo(
         noteImageName: 'assets/note_images/Tr_C.jpeg',
         noteName: 'Clef C',
-        noteSound: 'note_c.wav'),
+        noteSound: 'note_middle_c.wav'),
     HelperNoteInfo(
         noteImageName: 'assets/note_images/Tr_D.jpeg',
         noteName: 'Clef D',
-        noteSound: 'note_d.wav'),
+        noteSound: 'note_middle_d.wav'),
     HelperNoteInfo(
         noteImageName: 'assets/note_images/Tr_E.jpeg',
         noteName: 'Clef E',
-        noteSound: 'note_e.wav'),
+        noteSound: 'note_middle_e.wav'),
     HelperNoteInfo(
         noteImageName: 'assets/note_images/Tr_F.jpeg',
         noteName: 'Clef F',
-        noteSound: 'note_f.wav'),
+        noteSound: 'note_middle_f.wav'),
     HelperNoteInfo(
         noteImageName: 'assets/note_images/Tr_G.jpeg',
         noteName: 'Clef G',
-        noteSound: 'note_g.wav'),
+        noteSound: 'note_middle_g.wav'),
   ],
 );
