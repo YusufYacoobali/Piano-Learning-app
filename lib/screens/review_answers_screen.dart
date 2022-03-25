6be1651b522907fcd4bc7cb3579374a4186968fc
“Yingtong <yingtong.hu@kcl.ac.uk>
2022-03-25 16:58:59 +0000

    Make a rounded border for review question images.

--- lib/screens/review_answers_screen.dart
+++ lib/screens/review_answers_screen.dart
@@ -46,14 +46,17 @@ class _ReviewAnswersScreenState extends State<ReviewAnswersScreen> {
   Widget addQuestionImage(int questionIndex) {
     NextNoteNotifier _nextNote = NextNoteNotifier();
     _nextNote.setNextNote(questionBrain.getSpecificNote(questionIndex));
+    MusicSheet sheet =
+        MusicSheet(_nextNote, questionBrain.getSpecificClef(questionIndex));
+    sheet.changeToRoundedBorder();
+
     return ClipRRect(
       borderRadius: BorderRadius.circular(15.0),
       child: SizedBox(
         height: 200.0,
         width: 240.0,
         child: CustomPaint(
-          painter: MusicSheet(
-              _nextNote, questionBrain.getSpecificClef(questionIndex)),
+          painter: sheet,
           child: Container(),
         ),
       ),
