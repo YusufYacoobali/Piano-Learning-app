c009944391d6943e9efcc861bb5cdd429587935b
Nannaphat Keamanuchet <nannaphat.keamanuchet@kcl.ac.uk>
2022-03-25 22:17:27 +0000

    Prepare for new tests in review_answers_screen_test.dart - delete unused test - add prototype of new test to check all answer cards are created

--- test/review_answers_screen_test.dart
+++ test/review_answers_screen_test.dart
@@ -1,10 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
+//import 'package:sight_reading_app/lessons_and_quizzes/question_finder.dart';
 //mport 'package:sight_reading_app/constants.dart';
 import 'package:sight_reading_app/question_brain.dart';
 import 'package:sight_reading_app/screens/results_screen.dart';
 //import 'package:sight_reading_app/screens/review_answers_screen.dart';
 import 'package:sight_reading_app/questions.dart';
+//import 'package:sight_reading_app/screens/review_answers_screen.dart';
 
 void main() {
   testWidgets('Check that the review answers screen is being displayed',
@@ -21,24 +23,34 @@ void main() {
     await tester.pumpAndSettle();
     expect(find.text('Review Answers'), findsOneWidget);
   });
+
+  /*testWidgets('Check that all result cards are generated',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: ResultsScreen(
+            title: "Congratulations!",
+            score: 0.42,
+            questionBrain: QuestionBrain(questions: questions)),
+      ),
+    );
+    await tester.tap(find.text('Review Answers'));
+    await tester.pumpAndSettle();
+
+    // Tests every button.
+    for (Key resultCardKey in resultCardKeys) {
+      // Scrolls the screen to reach the desired widget.
+      await tester.scrollUntilVisible(
+        find.byKey(resultCardKey),
+        500.0,
+        scrollable: find.byType(Scrollable),
+      );
+      expect(find.byKey(resultCardKey), findsOneWidget);
+    }
+  });*/
   // TODO: Test each lesson card to see if all contents are there
   // Test result
   // test correct answer
   // test user answer
   // TODO: Test that there are the exact amount of cards available according to number of questions
-
-  // Check this to get the review answers screen
-
-  // testWidgets('Check that the "Exit" button takes the user to the menu page',
-  //     (WidgetTester tester) async {
-  //   await tester.pumpWidget(
-  //     const MaterialApp(
-  //       home: ResultsScreen(title: "Congratulations!", score: 0.42),
-  //     ),
-  //   );
-  //   await tester.tap(find.text('Exit'));
-  //   await tester.pumpAndSettle();
-  //   //expect(find.text('Read That Sheet'), findsOneWidget);
-  //   expect(find.byType(MenuScreen), findsOneWidget);
-  // });
 }
