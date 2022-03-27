5680bc722c9ac86ff1ab6b91a9ff9bfbaa0c604e
Nannaphat Keamanuchet <nannaphat.keamanuchet@kcl.ac.uk>
2022-03-27 15:14:07 +0100

    Add images and more cards - Add image of notes and more cards of different types of notes

--- lib/components/helper/helper_note_type_list.dart
+++ lib/components/helper/helper_note_type_list.dart
@@ -9,12 +9,12 @@ import 'helper_note_info.dart';
 
 final HelperList noteTypeList = HelperList(helperId: 3, helperList: [
   HelperNoteInfo(
-      noteImageName: Note(name: 'C4', duration: 4),
+      noteImageName: Note(name: 'C4', duration: 1),
       noteName: 'Quarter Note',
       noteSound: '',
       description: 'This note has length of 1 beat.'),
   HelperNoteInfo(
-      noteImageName: Note(name: 'C4', duration: 4),
+      noteImageName: Note(name: 'C4', duration: 2),
       noteName: 'Half Note',
       noteSound: '',
       description:
@@ -26,9 +26,21 @@ final HelperList noteTypeList = HelperList(helperId: 3, helperList: [
       description:
           'This note has a length of 4 beats. Equivalent to 4 quarter notes.'),
   HelperNoteInfo(
-      noteImageName: Note(name: 'C4', duration: 4),
+      noteImageName: Note(name: 'C4', duration: 0.5),
       noteName: 'Eighth Note',
       noteSound: '',
       description:
           'This note has a length of half a beat. 2 eighth notes is equivalent to a quarter note.'),
+  HelperNoteInfo(
+      noteImageName: Note(name: 'C4', duration: 1.5),
+      noteName: 'Dotted Note',
+      noteSound: '',
+      description:
+          'The dot behind adds half the beat value of the note in front. This note has a length of 1.5 beats.'),
+  HelperNoteInfo(
+      noteImageName: Note(name: 'C4', duration: 3),
+      noteName: 'Dotted Note',
+      noteSound: '',
+      description:
+          'In this case, the dotted note has a length of 2 + 1 = 3 beats.'),
 ]);
