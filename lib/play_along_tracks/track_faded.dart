3889b85cc7e2f1f68a509a8d09959adc92a19de2
janiesrosario <janies.rosario@kcl.ac.uk>
2022-03-30 14:11:46 +0100

    shorten track

--- lib/play_along_tracks/track_faded.dart
+++ lib/play_along_tracks/track_faded.dart
@@ -27,51 +27,6 @@ final Map<int, Note> _map = {
   14: Note(name: 'C#4', duration: 0.5),
   15: Note(name: 'C#4', duration: 0.5),
   16: Note(name: 'D4', duration: 0.5),
-
-
-  17: Note(name: 'G4', duration: 0.5), //b.1
-  18: Note(name: 'G4', duration: 0.5),
-  19: Note(name: 'E4', duration: 0.5),
-  20: Note(name: 'G4', duration: 0.5),
-  21: Note(name: 'E4', duration: 0.5),
-  22: Note(name: 'G4', duration: 0.5),
-  23: Note(name: 'A4', duration: 0.5),
-  24: Note(name: 'B4', duration: 0.5), //b.2
-  25: Note(name: 'G4', duration: 0.5),
-  26: Note(name: 'G4', duration: 0.5),
-  27: Note(name: 'D4', duration: 0.5),
-  28: Note(name: 'B4', duration: 1),
-  29: Note(name: 'G4', duration: 2), //b.3
-  //break
-  31: Note(name: 'G4', duration: 0.5),
-  32: Note(name: 'G4', duration: 0.5),
-  33: Note(name: 'G4', duration: 0.5),
-  34: Note(name: 'F#4', duration: 0.5), //b.4
-  35: Note(name: 'E4', duration: 0.5),
-  36: Note(name: 'F#4', duration: 0.5),
-  37: Note(name: 'F#4', duration: 0.5),
-  38: Note(name: 'E4', duration: 0.5),
-  39: Note(name: 'G4', duration: 1),
-  // start of phrase two
-  41: Note(name: 'G4', duration: 0.5),
-  42: Note(name: 'G4', duration: 0.5),
-  43: Note(name: 'E4', duration: 0.5),
-  44: Note(name: 'G4', duration: 0.5),
-  45: Note(name: 'E4', duration: 0.5),
-  46: Note(name: 'G4', duration: 0.5),
-  47: Note(name: 'A4', duration: 0.5), // b.5
-  48: Note(name: 'B4', duration: 1),
-  49: Note(name: 'G4', duration: 0.5),
-  50: Note(name: 'G4', duration: 0.5),
-  51: Note(name: 'D4', duration: 0.5),
-  52: Note(name: 'B4', duration: 3), // b.6
-  55: Note(name: 'G4', duration: 0.5),
-  56: Note(name: 'G4', duration: 0.5),
-  //start of chorus
-  57: Note(name: 'B4', duration: 0.5), // b.7
-  58: Note(name: 'B4', duration: 0.5),
-  59: Note(name: 'B4', duration: 0.5),
-
 };
 
 Map<int, Note> getTrack(){
