88a7141554e869e42cff46d138e3535041f98344
janiesrosario <janies.rosario@kcl.ac.uk>
2022-03-30 14:08:26 +0100

    new notes

--- lib/play_along_tracks/track_faded.dart
+++ lib/play_along_tracks/track_faded.dart
@@ -11,22 +11,22 @@ const Clef _clef = Clef.treble;
 const List<int> _difficultyBpm = [65,75,90];
 
 final Map<int, Note> _map = {
-  1:Note(name: 'G4', duration: 0.5), // b.1
-  2: Note(name: 'G4', duration: 0.5),
-  3: Note(name: 'G4', duration: 0.5),
-  4: Note(name: 'B4', duration: 0.5),
-  5: Note(name: 'E4', duration: 0.5), // b.2
-  6: Note(name: 'E4', duration: 0.5),
-  7: Note(name: 'E4', duration: 0.5),
-  8: Note(name: 'B4', duration: 0.5),
-  9: Note(name: 'B4', duration: 0.5), // b.3
-  10: Note(name: 'B4', duration: 0.5),
-  11: Note(name: 'B4', duration: 0.5),
-  12: Note(name: 'B4', duration: 0.5),
-  13: Note(name: 'F#4', duration: 0.5), // b.4
-  14: Note(name: 'F#4', duration: 0.5),
-  15: Note(name: 'F#4', duration: 0.5),
-  16: Note(name: 'E4', duration: 0.5),
+  1: Note(name: 'D4', duration: 0.5), // b.1
+  2: Note(name: 'D4', duration: 0.5),
+  3: Note(name: 'D4', duration: 0.5),
+  4: Note(name: 'F#4', duration: 0.5),
+  5: Note(name: 'B4', duration: 0.5), // b.2
+  6: Note(name: 'B4', duration: 0.5),
+  7: Note(name: 'B4', duration: 0.5),
+  8: Note(name: 'A4', duration: 0.5),
+  9: Note(name: 'F#4', duration: 0.5), // b.3
+  10: Note(name: 'F#4', duration: 0.5),
+  11: Note(name: 'F#4', duration: 0.5),
+  12: Note(name: 'F#4', duration: 0.5),
+  13: Note(name: 'C#4', duration: 0.5), // b.4
+  14: Note(name: 'C#4', duration: 0.5),
+  15: Note(name: 'C#4', duration: 0.5),
+  16: Note(name: 'D4', duration: 0.5),
 
 
   17: Note(name: 'G4', duration: 0.5), //b.1
