7f27d035c593405fc4c230c684f13e7435a16546
Pradeep Shah <pradeep.shah@kcl.ac.uk>
2022-04-03 18:05:55 +0100

    Fix test

--- test/quiz_selection_screen_test.dart
+++ test/quiz_selection_screen_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:sight_reading_app/constants.dart';
 import 'package:sight_reading_app/main.dart';
 import 'package:sight_reading_app/screens/menu_screen.dart';
 import 'package:sight_reading_app/screens/practice_screen.dart';
@@ -19,65 +20,67 @@ void main() {
 
   testWidgets(
       'Check that the quiz selection page is displayed when the button to go to the screen is pressed.',
-          (WidgetTester tester) async {
-        await _goToQuizSelectionScreen(tester);
-        expect(find.byType(QuizSelectionScreen), findsOneWidget);
-      });
+      (WidgetTester tester) async {
+    await _goToQuizSelectionScreen(tester);
+    expect(find.byType(QuizSelectionScreen), findsOneWidget);
+  });
 
   testWidgets('Check that the instruction text is displayed.',
-          (WidgetTester tester) async {
-        await _goToQuizSelectionScreen(tester);
-        expect(find.text('Choose a quiz:'),
-            findsOneWidget); //TODO: Would need to re-write to support multiple languages
-      });
+      (WidgetTester tester) async {
+    await _goToQuizSelectionScreen(tester);
+    expect(find.text('Choose a quiz:'),
+        findsOneWidget); //TODO: Would need to re-write to support multiple languages
+  });
 
   testWidgets(
       'Check that the settings icon is present on the quiz selection screen.',
-          (WidgetTester tester) async {
-        await _goToQuizSelectionScreen(tester);
-        expect(find.byKey(AppBarWithSettingsIcon.navigateToSettingsButtonKey),
-            findsOneWidget);
-      });
+      (WidgetTester tester) async {
+    await _goToQuizSelectionScreen(tester);
+    expect(find.byKey(AppBarWithSettingsIcon.navigateToSettingsButtonKey),
+        findsOneWidget);
+  });
 
   testWidgets(
       'Check that the settings icon navigates to the settings screen once clicked.',
-          (WidgetTester tester) async {
-        await _goToQuizSelectionScreen(tester);
-        await tester
-            .tap(
-            find.byKey(AppBarWithSettingsIcon.navigateToSettingsButtonKey));
-        await tester.pumpAndSettle();
-        expect(find.byType(SettingsScreen), findsOneWidget);
-      });
+      (WidgetTester tester) async {
+    await _goToQuizSelectionScreen(tester);
+    await tester
+        .tap(find.byKey(AppBarWithSettingsIcon.navigateToSettingsButtonKey));
+    await tester.pumpAndSettle();
+    expect(find.byType(SettingsScreen), findsOneWidget);
+  });
 
   testWidgets('Check that all the ListView quiz buttons are generated',
-          (WidgetTester tester) async {
-        await _goToQuizSelectionScreen(tester);
-        for (Key quizButtonKey in quizButtonKeys) {
-          await tester.scrollUntilVisible(
-            find.byKey(quizButtonKey),
-            500.0,
-            scrollable: find.byType(Scrollable),
-          );
-          await tester.pumpAndSettle();
-          expect(find.byKey(quizButtonKey), findsOneWidget);
-        }
-      });
+      (WidgetTester tester) async {
+    await _goToQuizSelectionScreen(tester);
+    for (Key quizButtonKey in quizButtonKeys) {
+      await tester.scrollUntilVisible(
+        find.byKey(quizButtonKey),
+        500.0,
+        scrollable: find.byType(Scrollable),
+      );
+      await tester.pumpAndSettle();
+      expect(find.byKey(quizButtonKey), findsOneWidget);
+    }
+  });
 
   testWidgets('Check that the quiz buttons display the correct text',
-          (WidgetTester tester) async {
-        await _goToQuizSelectionScreen(tester);
-        for (var i = 0; i < quizButtonKeys.length; i++) {
-          await tester.scrollUntilVisible(
-            find.byKey(quizButtonKeys[i]),
-            500.0,
-            scrollable: find.byType(Scrollable),
-          );
-          await tester.pumpAndSettle();
-          expect(find.text(quizzes[i]), findsWidgets);
-          expect(find.text('Record: ${quizRecords[i]}'), findsWidgets);
-        }
-      });
+      (WidgetTester tester) async {
+    await _goToQuizSelectionScreen(tester);
+    for (var i = 0; i < quizButtonKeys.length; i++) {
+      await tester.scrollUntilVisible(
+        find.byKey(quizButtonKeys[i]),
+        500.0,
+        scrollable: find.byType(Scrollable),
+      );
+      await tester.pumpAndSettle();
+      expect(find.text(quizzes[i]), findsWidgets);
+      expect(
+          find.text(
+              'Record: ${quizRecords[i]} / $numOfQuestionsInPracticeQuiz'),
+          findsWidgets);
+    }
+  });
 
   //TODO: Fix
   // testWidgets(
@@ -92,39 +95,39 @@ void main() {
   // );
 
   testWidgets('Check that the ${"random quiz button"} is displayed on screen.',
-          (WidgetTester tester) async {
-        await _goToQuizSelectionScreen(tester);
-        expect(find.byKey(randomQuizSelectedKey), findsOneWidget);
-      });
+      (WidgetTester tester) async {
+    await _goToQuizSelectionScreen(tester);
+    expect(find.byKey(randomQuizSelectedKey), findsOneWidget);
+  });
 
   testWidgets(
       'Check that the ${"random quiz button"} displays the correct text',
-          (WidgetTester tester) async {
-        await _goToQuizSelectionScreen(tester);
-        expect(find.text('Random mixed quiz'), findsOneWidget);
-      });
+      (WidgetTester tester) async {
+    await _goToQuizSelectionScreen(tester);
+    expect(find.text('Random mixed quiz'), findsOneWidget);
+  });
 
   testWidgets('Check that scrolling does not move the random quiz button',
-          (WidgetTester tester) async {
-        await _goToQuizSelectionScreen(tester);
-        final buttonFinder = find.byKey(randomQuizSelectedKey);
-        final startPos = tester.getCenter(buttonFinder);
-        final gesture = await tester.startGesture(const Offset(0, 300));
-        await gesture.moveBy(const Offset(0, -300)); //Scrolls the screen
-        await tester.pumpAndSettle();
-        expect(
-            startPos,
-            equals(tester.getCenter(
-                buttonFinder))); //Checks that the position of the button has not changed.
-      });
+      (WidgetTester tester) async {
+    await _goToQuizSelectionScreen(tester);
+    final buttonFinder = find.byKey(randomQuizSelectedKey);
+    final startPos = tester.getCenter(buttonFinder);
+    final gesture = await tester.startGesture(const Offset(0, 300));
+    await gesture.moveBy(const Offset(0, -300)); //Scrolls the screen
+    await tester.pumpAndSettle();
+    expect(
+        startPos,
+        equals(tester.getCenter(
+            buttonFinder))); //Checks that the position of the button has not changed.
+  });
 
   testWidgets(
       'Check that clicking the random quiz button navigates you to a random quiz screen.',
-          (WidgetTester tester) async {
-        await _goToQuizSelectionScreen(tester);
-        await tester.tap(find.byKey(randomQuizSelectedKey));
-        await tester.pumpAndSettle();
-        //TODO: Fix.
-        // expect(find.byType(RandomQuizScreen), findsOneWidget);
-      });
+      (WidgetTester tester) async {
+    await _goToQuizSelectionScreen(tester);
+    await tester.tap(find.byKey(randomQuizSelectedKey));
+    await tester.pumpAndSettle();
+    //TODO: Fix.
+    // expect(find.byType(RandomQuizScreen), findsOneWidget);
+  });
 }
